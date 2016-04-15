125b6d72f92157e466d7d317fa02a3257c1619f0
Dr0virus <e.nourifard110@gmail.com>
2016-04-15 13:28:19 +0430

    Create autoleave.lua

---
+++ plugins/autoleave.lua
@@ -0,0 +1,21 @@
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg)
+  if msg.service and msg.action.type == 'chat_add_user' or msg.action.type == 'channel_add_user' then
+    local data = load_data(_config.moderation.data)
+    if not data[tostring(msg.to.id)] then
+      print "This is not one of my groups Bye. Leaving..."
+      channel_kick_user('channel#id'..msg.to.id, 'user#id'..our_id, callback, false)
+    end
+  end
+end
+
+return {
+  patterns = {
+    "^!!tgservice (.+)$"
+  },
+  run = run
+}
