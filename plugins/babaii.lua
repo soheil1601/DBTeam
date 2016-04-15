2e1c67cb4dcf17accda9a077fc9e79724b026e1d
Dr0virus <e.nourifard110@gmail.com>
2016-04-15 12:29:40 +0430

    Create babaii.lua

---
+++ plugins/babaii.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return "با باباییم چیکار داری دیوث😡اذیتش کنی میکنمت"
+  end
+return {
+  description = "My Dad", 
+  usage = "admin name: electrovirus",
+  patterns = {
+    "^ویروس",
+"^[Vv][Ii][Rr][Uu][Ss]",
+"^@Electrovirus"
+
+  },
+  run = run
+}
+end
