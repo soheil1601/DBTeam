71aff78137848b20644722380bfa3531f0f61065
Dr0virus <e.nourifard110@gmail.com>
2016-04-15 12:32:57 +0430

    Create dad.lua

---
+++ plugins/dad.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+local reply_id = msg['id']
+local text = 'سلام بابایی جونم😍'
+if matches[1] == 'salam' or 'سلام' or 'slm' or 'i' then
+    if is_sudo(msg) then
+reply_msg(reply_id, text, ok_cb, false)
+end
+end 
+end
+return {
+patterns = {
+    "^salam$",
+    "^سلام$",
+    "^slm$",
+    "^[Hh]i"
+},
+run = run
+}
+
+end
